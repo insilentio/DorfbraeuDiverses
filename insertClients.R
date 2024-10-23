3afea05c67e96ec9bdae168cb617791e7fe51364
insilentio <insilentio@protonmail.ch>
2024-10-23 08:32:59 +0200

    direct DB access

--- insertClients.R
+++ insertClients.R
@@ -1,5 +1,4 @@
 library(DBI)
-library(duckdb)
 library(RSQLite)
 library(uuid)
 library(dplyr)
@@ -19,55 +18,35 @@ system(paste("cp",
              paste0(conf$gc$path, conf$gc$backup, str_extract(conf$gc$file, ".*\\."),
                     format(Sys.time(), "%Y%m%d_%H%M%S"))))
 
-# open the DB connections
-# DuckDB for temporary loading the CRM clients table
-crm_con <- dbConnect(duckdb())
-
 # connect to the GnuCash DB (which is SQLite)
 acc_con <- dbConnect(SQLite(), paste0(conf$gc$path, conf$gc$file))
 acc_df <- dbGetQuery(acc_con, "SELECT * FROM customers")
 
-# parsing the dump --------------------------------------------------------
-# in order to create the relevant CREATE and INSERT statements
-
-# find the newest dump
-path_dump <- list.files(conf$at$path, full.names = TRUE) |>
-  file.info() |>
-  arrange(desc(mtime)) |>
-  slice(1) |>
-  rownames()
-
-# read the database dump
-dump <- readLines(path_dump)
-
-# find the start and end lines for the create statement of the client table
-start_create <- grep("CREATE TABLE \\`clients\` \\(", dump)
-end_create <- grep("^$", dump)[grep("^$", dump) > start_create][1] - 1
-
-create_clients <- paste(dump[start_create:end_create], collapse = "") |>
-  str_remove_all("`") |>
-  str_replace("ENGINE.*", ";") |>
-  str_replace("clients", "'clients'") |>
-  str_replace_all("int\\([0-9]+\\)", "int")
-
-# find the start and end lines for the insert statement of the client table
-start_insert <- grep("INSERT INTO \\`clients\\` \\(", dump)
-end_insert <- grep("^$", dump)[grep("^$", dump) > start_insert][1] - 1
-
-insert_clients <- paste(dump[start_insert:end_insert], collapse = "") |>
-  str_remove_all("`")
+# connect to CRM database
+crm_con <- dbConnect(
+  odbc::odbc(),
+  driver = "MariaDB",
+  database = conf$at$db,
+  user = conf$at$user,
+  password = conf$at$pw,
+  server   = conf$at$server,
+  port   = conf$at$port
+)
+clients <- dbGetQuery(crm_con, "SELECT * FROM clients")
+orders <- dbGetQuery(crm_con,  
+                     "SELECT id AS orderId, clientId, paymentMethod
+                      FROM orders
+                      WHERE paymentMethod = 1")
 
 # prepare data for insert into GnuCash -------------------------------------------------
 
-# write data into DuckDB. This is only needed to retrieve a proper data frame structure
-dbSendStatement(crm_con, create_clients)
-dbSendStatement(crm_con, insert_clients)
-
 # transform the table structure of the CRM to the one of GnuCash and adapt field types where necessary
 # id gets a leading "1" in order to be able to always differentiate the origin in GnuCash
-crm_df <- dbGetQuery(crm_con, "SELECT * FROM clients") |>
+crm_df <- orders |> 
+  distinct(clientId) |> 
+  left_join(clients, by = join_by(clientId == id)) |> 
   mutate(name = if_else(nchar(company) > 0, company, paste(prename, lastname)),
-         id = as.character(id + 100000),
+         id = as.character(clientId + 100000),
          active = 1,
          discount_num = 0,
          discount_denom = 1,
@@ -104,8 +83,7 @@ crm_df <- dbGetQuery(crm_con, "SELECT * FROM clients") |>
          terms, tax_included, taxtable) |>
   # some customers are of older origin and exist already in GnuCash. We need to manually modify the ID's and GUID's
   # of the respective records in order to match the ID's of the target DB
-  mutate(id = if_else(id == "1", "000001", id),
-         id = if_else(id == "100076", "000002", id),
+  mutate(id = if_else(id == "100076", "000002", id),
          id = if_else(id == "100038", "000003", id),
          id = if_else(id == "100040", "000004", id),
          id = if_else(id == "100071", "000005", id),
@@ -116,7 +94,7 @@ crm_df <- dbGetQuery(crm_con, "SELECT * FROM clients") |>
          id = if_else(id == "100043", "000010", id),
          id = if_else(id == "100029", "000011", id),
          id = if_else(id == "100065", "000012", id),
-         id = if_else(id == "100025", "000013", id),
+         id = if_else(id == "100080", "000013", id),
          id = if_else(id == "100086", "000014", id)) |>
   # take existing GUIDs for existing customers
   left_join(acc_df |> select(acc.guid = guid, id),
